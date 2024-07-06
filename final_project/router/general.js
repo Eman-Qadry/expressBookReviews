32a898536e809e5b94db8f7efc69f978b2c08583
Eman-Qadry <wafaakadry756@gmail.com>
2024-07-06 13:33:59 +0300

    get all booksbased on ISBN using async and promises is completed

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -1,5 +1,6 @@
 const express = require('express');
 let books = require("./booksdb.js");
+const axios = require('axios');
 let isValid = require("./auth_users.js").isValid;
 let users = require("./auth_users.js").users;
 const public_users = express.Router();
@@ -37,17 +38,48 @@ public_users.get('/', async (req, res) => {
       return res.status(500).json({ message: error });
   }
 });
-
-// Get book details based on ISBN
-public_users.get('/isbn/:isbn', (req, res) => {
-    const { isbn } = req.params;
-    if (!books[isbn]) {
-        return res.status(404).json({ message: "Book not found" });
-    }
-
-    return res.status(200).json(books[isbn]);
+public_users.get('/promise', (req, res) => {
+  fetchBooks()
+      .then(bookList => {
+          return res.status(200).json(bookList);
+      })
+      .catch(error => {
+          return res.status(500).json({ message: error });
+      });
 });
 
+const fetchBookByISBN = (isbn) => {
+  return new Promise((resolve, reject) => {
+      if (books[isbn]) {
+          resolve(books[isbn]);
+      } else {
+          reject("Book not found");
+      }
+  });
+}
+
+// Get book details based on ISBN using async-await
+public_users.get('/isbn/:isbn', async (req, res) => {
+  const { isbn } = req.params;
+  try {
+      const book = await fetchBookByISBN(isbn);
+      return res.status(200).json(book);
+  } catch (error) {
+      return res.status(404).json({ message: error });
+  }
+});
+
+// Get book details based on ISBN using Promise callbacks
+public_users.get('/promise/isbn/:isbn', (req, res) => {
+  const { isbn } = req.params;
+  fetchBookByISBN(isbn)
+      .then(book => {
+          return res.status(200).json(book);
+      })
+      .catch(error => {
+          return res.status(404).json({ message: error });
+      });
+});
 // Get book details based on author
 public_users.get('/author/:author', (req, res) => {
     const { author } = req.params;
